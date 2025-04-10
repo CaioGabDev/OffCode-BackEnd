4322a8ae1542b1bc758fe310589659808e6bd5f5
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-10 14:08:28 -0300

    feat: create comentarios table

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -38,3 +38,22 @@ INSERT INTO posts (id_usuario, conteudo_post, anexo, data_publicacao) VALUES
 (4, 'Lorem ipsum dolor sit amet.', 'https://avatars.githubusercontent.com', '2025-07-23'),
 (5, 'Lorem ipsum dolor sit amet.', 'https://avatars.githubusercontent.com', '2023-01-01'),
 (6, 'Lorem ipsum dolor sit amet.', 'https://avatars.githubusercontent.com', '2023-01-01');
+
+CREATE TABLE comentarios (
+    id_comentario SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL, --FK
+    id_post INT NOT NULL, --FK
+    conteudo_comentario TEXT NOT NULL,
+    anexo VARCHAR(255), 
+    data_publicacao DATE,
+    CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+    CONSTRAINT fk_post FOREIGN KEY (id_post) REFERENCES posts(id_post) 
+);
+
+INSERT INTO comentarios (id_usuario, id_post, conteudo_comentario, anexo, data_publicacao) VALUES
+(1, 2, 'amei seu post. ajudou muito', NULL, '2025-07-23'),
+(2, 1, 'amei seu post. ajudou muito', NULL, '2025-07-23'),
+(3, 4, 'amei seu post. ajudou muito', NULL, '2025-07-23'),
+(4, 3, 'amei seu post. ajudou muito', NULL, '2025-07-23'),
+(5, 6, 'amei seu post. ajudou muito', NULL, '2025-07-23'),
+(6, 5, 'amei seu post. ajudou muito', NULL, '2025-07-23'),
