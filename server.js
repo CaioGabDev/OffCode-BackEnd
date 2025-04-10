12e77c25ef3a71edf35a50d1ec63e09e404048cd
Caio Lacerda <caio.g.silva20@aluno.senai.br>
2025-04-10 13:29:35 -0300

    feat: create structure files

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const usersRoutes = require("./src/routes/usersRoutes");
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", usersRoutes);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
