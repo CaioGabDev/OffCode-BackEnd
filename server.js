0f6a040f17164bd4ef3db743ca7a726de37d7a7e
Bernardo Marques <sardinhaber@gmail.com>
2025-04-24 13:29:24 -0300

    feat: install swagger and multer, add config swagger

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./src/routes/userRoutes");
 const likeRoutes = require("./src/routes/likeRoutes");
 const postRoutes = require("./src/routes/postRoutes");
 const commentRoutes = require("./src/routes/commentRoutes");
+const setupSwagger = require('./src/config/swagger');
 
 
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+setupSwagger(app);
 
 app.use("/api", userRoutes);
 app.use("/api", likeRoutes );
---
+++ src/config/swagger.js
@@ -0,0 +1,22 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API do APP Offcode',
+      version: '1.0.0',
+      description: 'Documentação da API para gerenciar uma rede social',
+    },
+  },
+  apis: ['../src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+const setupSwagger = (app) => {
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = setupSwagger;
