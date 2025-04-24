2859cff997aaa9d950bf37c40a8809d18c7f2c15
Bernardo Marques <sardinhaber@gmail.com>
2025-04-24 13:46:40 -0300

    feat: add swagger in route getAll in posts

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -4,7 +4,26 @@ const router = express.Router();
 
 const postController = require("../controllers/postController");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Posts
+ *   description: Gerenciamento de posts
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Lista todos os posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: Lista de posts
+ */
 router.get("/post", postController.getAllPosts);
+
+
 router.get("/post/:id", postController.getById);
 router.post("/post", postController.createPost);
 router.put("/post/:id", postController.updatePost);
